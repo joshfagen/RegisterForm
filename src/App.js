7f650b8cff1c4a2c90501fb2d2d571d824f6d382
Josh <joshmfagen@gmail.com>
2022-08-14 19:10:59 -0400

    adds comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //Set initial values for the inputs and states.
   const initValues = { email: '', password: '', confirmPassword: ''};
   const [formValues, setFormValues] = useState(initValues);
   const [formErrors, setFormErrors] = useState({});
@@ -9,27 +10,33 @@ function App() {
   const [passwordShown, setPasswordShown] = useState(false);
   
 
+  // adjust values on change in inputs
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({...formValues, [name]: value });
   }
 
+  //handle submission, check entered values for errors
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
     setSubmitCheck(true)
   }
 
+  //validate inputs
   const validate = (formValues) => {
     const errors = {};
 
+    //regex string for email
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
+    //email validation
     if(!formValues.email) {
       errors.email = "Email is required"
     } else if(!regex.test(formValues.email)){
       errors.email = 'Please use valid email address'
     }
+    //password and confirm password validation
     if(!formValues.password){
       errors.password = "Password is required"
     } else if(formValues.password.length < 6){
@@ -57,9 +64,11 @@ function App() {
       errors.confirmPassword = "Passwords must match";
     }
 
+    
     return errors;
   }
 
+  //hide or display password when icon clicked
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   }
